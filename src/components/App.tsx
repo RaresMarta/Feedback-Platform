0dfe7f2f7c4da181b8a31fce2e6084d626ca7543
Rares Marta <rares.marta14@gmail.com>
2025-03-23 02:23:00 +0200

    commit

---
+++ src/components/App.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import Container from "./layout/Container";
+import Footer from "./layout/Footer";
+import HashtagList from "./HashtagList";
+import { TFeedbackItem } from "../lib/types";
+
+function App() {
+  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleAddToList = async (text: string) => {
+    const companyName = text
+      .split(" ")
+      .find((word) => word.includes("#"))!
+      .substring(1);
+
+    const newItem: TFeedbackItem = {
+      id: new Date().getTime(),
+      text: text,
+      upvoteCount: 0,
+      daysAgo: 0,
+      company: companyName,
+      badgeLetter: companyName.substring(0, 1).toUpperCase(),
+    };
+
+    setFeedbackItems([...feedbackItems, newItem]);
+
+    await fetch(
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
+      {
+        method: "POST",
+        body: JSON.stringify(newItem),
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+      }
+    );
+  };
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessage("Something went wrong.");
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchFeedbackItems();
+  }, []);
+
+  return (
+    <div className="app">
+      <Container
+        feedbackItems={feedbackItems}
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        handleAddToList={handleAddToList}
+      />
+
+      <HashtagList />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
